551fe605645a00c17ca584721dab677710684e20
Richin Jain <rijai@microsoft.com>
2022-10-19 07:24:54 -0700

    Removing unused packages and cleaning up config

--- src/Luna.UI/enduser_client/public/config.js
+++ src/Luna.UI/enduser_client/public/config.js
@@ -1,7 +1,8 @@
 var Configs = {
     API_ENDPOINT: "https://lunatest-apiapp.azurewebsites.net/api/",
-    ISV_NAME: "Microsoft",
-    AAD_APPID: "50a6b69d-325a-4c02-aff3-51703d904789",
+    ISV_NAME: "CONTOSO",
+    AAD_APPID: "YOUR_AAD_APP_ID",
+    AAD_TENANT_ID: "YOUR_AAD_TENANT_ID",
     AAD_ENDPOINT: "https://lunatest-isvapp.azurewebsites.net",
     HEADER_BACKGROUND_COLOR: "#118811"
 }
